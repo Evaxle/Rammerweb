0075dc940ca2e8df9f1d6c5c5524a341efc9cc23
Evan <128929890+Evaxle@users.noreply.github.com>
2025-06-02 22:18:52 +0000

    This is rigged

--- src/util/StrShuffler.js
+++ src/util/StrShuffler.js
@@ -1,15 +1,27 @@
+/*
+
+baseDictionary originally generated with (certain characters was removed to avoid breaking pages):
+
+let str = '';
+for (let i = 32; i <= 126; i++) {
+  let c = String.fromCharCode(i);
+  if (c !== '/' && c !== '_' && encodeURI(c).length === 1) str += c;
+}
+
+*/
+
 const mod = (n, m) => ((n % m) + m) % m;
-const baseDictionary = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz~-";
-const shuffledIndicator = "_rhs";
+const baseDictionary = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz~-';
+const shuffledIndicator = '_rhs';
 const generateDictionary = function () {
-    let str = "";
-    const split = baseDictionary.split("");
+    let str = '';
+    const split = baseDictionary.split('');
     while (split.length > 0) {
         str += split.splice(Math.floor(Math.random() * split.length), 1)[0];
     }
     return str;
 };
-export default class StrShuffler {
+class StrShuffler {
     constructor(dictionary = generateDictionary()) {
         this.dictionary = dictionary;
     }
@@ -17,11 +29,11 @@ export default class StrShuffler {
         if (str.startsWith(shuffledIndicator)) {
             return str;
         }
-        let shuffledStr = "";
+        let shuffledStr = '';
         for (let i = 0; i < str.length; i++) {
             const char = str.charAt(i);
             const idx = baseDictionary.indexOf(char);
-            if (char === "%" && str.length - i >= 3) {
+            if (char === '%' && str.length - i >= 3) {
                 shuffledStr += char;
                 shuffledStr += str.charAt(++i);
                 shuffledStr += str.charAt(++i);
@@ -40,11 +52,11 @@ export default class StrShuffler {
 
         str = str.slice(shuffledIndicator.length);
 
-        let unshuffledStr = "";
+        let unshuffledStr = '';
         for (let i = 0; i < str.length; i++) {
             const char = str.charAt(i);
             const idx = this.dictionary.indexOf(char);
-            if (char === "%" && str.length - i >= 3) {
+            if (char === '%' && str.length - i >= 3) {
                 unshuffledStr += char;
                 unshuffledStr += str.charAt(++i);
                 unshuffledStr += str.charAt(++i);
@@ -57,3 +69,9 @@ export default class StrShuffler {
         return unshuffledStr;
     }
 }
+
+StrShuffler.baseDictionary = baseDictionary;
+StrShuffler.shuffledIndicator = shuffledIndicator;
+StrShuffler.generateDictionary = generateDictionary;
+
+module.exports = StrShuffler;
